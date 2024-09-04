bc177ef5ece7fac19686f6d688c1c69b779f1c73
Nathan Sorkin <NateSrkn@gmail.com>
2024-09-04 16:02:16 -0400

    remove unused packages and code

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,18 +1,9 @@
 import builder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import { BookShelfPagingObject, Weather } from "../types";
+import { BookShelfPagingObject } from "../types";
 import { client } from "@/sanity/lib/client";
 export const urlFor = (source: SanityImageSource) => {
   return builder(client).image(source);
-};
-
-export const getWeather = async () => {
-  const response: AxiosResponse<Weather> = await axios(
-    `http://api.openweathermap.org/data/2.5/weather?q=Columbus&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&units=imperial`,
-  );
-  return response.data;
 };
 
 export enum BookShelfEnum {
@@ -41,21 +32,6 @@ export const getBooksByShelf = async (
   }
   return response.json();
 };
-
-export const getBooks = async () => {
-  const response: Array<BookShelfPagingObject> = await Promise.all([
-    fetcher({
-      url: `https://www.googleapis.com/books/v1/users/110654047935577037763/bookshelves/0/volumes?key=${process.env.NEXT_PUBLIC_BOOKS_KEY}`,
-    }),
-    fetcher({
-      url: `https://www.googleapis.com/books/v1/users/110654047935577037763/bookshelves/3/volumes?key=${process.env.NEXT_PUBLIC_BOOKS_KEY}`,
-    }),
-  ]);
-  return response;
-};
-
-export const fetcher = (args: AxiosRequestConfig) =>
-  axios(args).then((r) => r.data);
 
 export const pullInitials = (name: string) => {
   const names = name ? name.split(" ") : [];
